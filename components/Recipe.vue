9b8e7380af28c214f329a0cdea96d32830f1d915
GIDEON PETERS <gideonpeters85@gmail.com>
2019-09-06 14:37:21 +0100

    made some changes

---
+++ components/Recipe.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<article class="recipe">
+			<div :style="{ backgroundImage: 'url(' + thumbnail + ')' }" class="thumbnail"></div>
+			<h1>{{title}}</h1>
+			<p>{{previewText}}</p>
+		</article>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['thumbnail', 'title', 'previewText']
+}
+</script>
+
+<style lang="stylus" scoped>
+.recipe {
+  box-sizing: border-box;
+  width: 280px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #aaa;
+  margin: 10px;
+}
+
+.thumbnail {
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 200px;
+}
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<section class="recipes">
+		<recipe v-for="(recipe, ix) in recipes" :key="ix" :thumbnail="recipe.imageLink" />
+	</section>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		recipes: [
+			{
+				title: 'Some food',
+				description: 'Some nice preview text',
+				imageLink:
+					'https://www.eatright.org/-/media/eatrightimages/food/nutrition/nutritionfactsandfoodlabels/sodium-salt-1083487948.jpg'
+			},
+			{
+				title: 'Some other food',
+				description: 'Some nice preview text',
+				imageLink:
+					'https://img.taste.com.au/K02TrrZJ/w720-h480-cfill-q80/taste/2016/11/rachel-87711-2.jpeg'
+			}
+		]
+	})
+}
+</script>
+
+<style scoped>
+.recipes {
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: center;
+	align-items: center;
+}
+</style>
